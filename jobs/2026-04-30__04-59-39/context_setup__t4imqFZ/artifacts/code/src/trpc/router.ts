2f70ae3aeeff3da7c2605d535c74b772292d3b7e
Wei Zhang <kweizh@tabbyml.com>
2026-04-30 18:15:15 +0800

    fix(jobs): drop kimi result

--- jobs/2026-04-30__04-59-39/context_setup__t4imqFZ/artifacts/code/src/trpc/router.ts
+++
@@ -1,9 +0,0 @@
-import { router, publicProcedure } from './init';
-
-export const appRouter = router({
-  getUser: publicProcedure.query(({ ctx }) => {
-    return { userId: ctx.userId };
-  }),
-});
-
-export type AppRouter = typeof appRouter;
--- jobs/2026-04-30__04-59-39/v11_nextjs_app_router_rsc__UiaJtPg/artifacts/code/src/app/ClientComponent.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { trpc } from '../utils/trpc';
-
-export default function ClientComponent() {
-  const { data, isLoading } = trpc.hello.useQuery({ name: 'Client' });
-  return <p id="client-data">{isLoading ? 'Loading...' : data}</p>;
-}
